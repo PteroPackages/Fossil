0eda4d471b7f8b551ca2a481f03683a2d7944c5c
devnote-dev <inferno0745@gmail.com>
2022-02-25 05:21:10 +0000

    feat(models): start implementing api models

---
+++ src/models.cr
@@ -0,0 +1,25 @@
+require "json"
+require "yaml"
+
+module Fossil::Models
+  abstract struct BaseModel
+    include JSON::Serializable
+    include YAML::Serializable
+  end
+
+  struct User < BaseModel
+    property id : Int64
+    property external_id : String?
+    property uuid : String
+    property username : String
+    property email : String
+    property first_name : String
+    property last_name : String
+    property language : String
+    property root_admin : Bool
+    @[JSON::Field(key: "2fa")]
+    property two_factor : Bool
+    property created_at : Time
+    property updated_at : Time?
+  end
+end
