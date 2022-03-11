505845b16a09fb2271d78d361321aa9a519746ca
devnote-dev <inferno0745@gmail.com>
2022-03-11 23:11:41 +0000

    test(spec): implement config spec

--- spec/fossil_spec.cr
+++ spec/fossil_spec.cr
@@ -1,9 +1,21 @@
 require "./spec_helper"
 
 describe Fossil do
-  # TODO: Write tests
+  options = Fossil::CmdOptions.new true, true, true, true
 
-  it "works" do
-    false.should eq(true)
+  it "sets the environment variable" do
+    {% if flag?(:win32) %}
+    `set FOSSIL_PATH=%CD%\\bin`
+    {% else %}
+    `export FOSSIL_PATH=$(pwd)/bin`
+    {% end %}
+  end
+
+  it "initializes the config" do
+    Fossil::Commands::ConfigSetup.new ["init"], options
+  end
+
+  it "fetches the config" do
+    Fossil::Commands::ConfigSetup.new ["show"], options
   end
 end
