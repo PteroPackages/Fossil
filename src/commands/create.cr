de47f4f9ee4703b2b0ffa7c69a81806a6bd373e7
devnote-dev <inferno0745@gmail.com>
2022-06-19 12:48:17 +0100

    fix(commands): attempt to fix ignored files

--- src/commands/create.cr
+++ src/commands/create.cr
@@ -15,7 +15,7 @@ module Fossil::Commands
 
       Usage:
           fossil create <server> [-n|--name <name>] [-l|--locked]
-                                  [-i|--ignored <file>] [-h|--help]
+                                  [-i|--ignored <files>] [-h|--help]
 
       Arguments:
           server                the identifier of the server
@@ -23,7 +23,7 @@ module Fossil::Commands
       Options:
           -n, --name <name>     set the name of the archive
           -l, --locked          lock the archive when created
-          -i, --ignored <file>
+          -i, --ignore <files>  a list of files to ignore (separated by ",")
           -h, --help            send help information
       HELP
 
@@ -35,7 +35,7 @@ module Fossil::Commands
       OptionParser.parse(args) do |parser|
         parser.on("-n <name>", "--name <name>", "set the name of the archive") { |v| @@name = v }
         parser.on("-l", "--locked", "lock the archive when created") { @@locked = true }
-        parser.on("-i <file>", "--ignored <file>", "") { |v| @@ignored = v }
+        parser.on("-i <files>", "--ignore <files>", %(a list of files to ignore (separated by ","))) { |v| @@ignored = v }
         parser.on("-h", "--help", "send help information") { send_help }
 
         parser.missing_option { |op| Log.fatal "missing option #{op} <...>" }
@@ -51,17 +51,19 @@ module Fossil::Commands
         end
       end
 
-      cfg = Commands::Config.read_config
+      cfg = Config.read_config
       http = Http.new cfg
-      backup = Http.new(cfg).create_backup @@server, @@name, @@locked, @@ignored
+      ignore = @@ignored.gsub ',', '\n'
+
+      backup = Http.new(cfg).create_backup @@server, @@name, @@locked, ignore
 
       Log.info [
         "created new archive",
-        "name: #{backup.name}",
-        "uuid: #{backup.uuid}",
-        "size: #{backup.bytes}",
-        "locked: #{backup.is_locked}",
-        "successful: #{backup.is_successful}"
+        "name:        #{backup.name}",
+        "uuid:        #{backup.uuid}",
+        "size:        #{backup.bytes}",
+        "locked:      #{backup.is_locked}",
+        "successful:  #{backup.is_successful}"
       ]
       exit
     end
