f12a9de0a3752438b04124f9bd0fe7407cb1ae27
devnote-dev <inferno0745@gmail.com>
2022-10-11 00:47:34 +0100

    feat(errors): add missing error messages

--- src/errors.cr
+++ src/errors.cr
@@ -2,27 +2,49 @@ module Fossil
   class Error < Exception
     enum Kind
       Uncaught
-      None
+      ConfigNotFound
+      ConfigNotSet
+      ConfigWriteDenied
+      InvalidConfigUrl
+      InvalidConfigKey
     end
 
     getter kind : Kind
 
-    def initialize(@kind : Kind, message : String)
-      super message
-    end
-
-    def initialize(@kind : Kind, cause : Exception)
+    def initialize(@kind : Kind, cause : Exception? = nil)
       super cause: cause
     end
 
     def format_log(fatal : Bool = false) : Nil
       case @kind
+      in Kind::ConfigNotFound
+        Log.error [
+          "The Fossil config file could not be found",
+          %(Run the '#{"fossil setup".colorize(:light_blue)}' command to solve)
+        ]
+      in Kind::ConfigNotSet
+        Log.error ["The panel URL or API key has not been set for the config"]
+        Log.notice %(See '#{"fossil config --help".colorize(:light_blue)}' for more information)
+      in Kind::ConfigWriteDenied
+        Log.error [
+          "Failed to write to config file: permission denied",
+          "Make sure you are running this command #{{{ flag?(:win32) ? "with admin permissions" : "as root user" }}}"
+        ]
+      in Kind::InvalidConfigUrl
+        Log.error [
+          "The panel URL set in the config is invalid",
+          %(Update this using the '#{"fossil config --set --url <url>".colorize(:light_blue)}' command)
+        ]
+      in Kind::InvalidConfigKey
+        Log.error [
+          "The API key set in the config is invalid",
+          %(Update this using the '#{"fossil config --set --key <key>".colorize(:light_blue)}' command)
+        ]
       in Kind::Uncaught
         Log.error [
           "An unexpected error occured, please report this to the PteroPackages team",
           "Context: #{message}"
         ] + inspect_with_backtrace.lines
-      in Kind::None
       end
 
       raise SystemExit.new if fatal
