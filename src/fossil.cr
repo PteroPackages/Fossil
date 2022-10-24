63d96709e35f8644cd6b91cb92bbe26a99ed05d9
devnote-dev <inferno0745@gmail.com>
2022-10-24 01:09:18 +0100

    chore: formatting

--- src/fossil.cr
+++ src/fossil.cr
@@ -32,22 +32,22 @@ module Fossil
       Fossil - Pterodactyl Archive Manager
 
       Usage:
-          fossil [options] <command> [arguments]
+              fossil [options] <command> [arguments]
 
       Commands:
-          list      lists existing archives
-          create    creates an archive from the panel
-          status    gets the status of an archive
-          restore   restores an archive to the panel
-          delete    deletes existing archives
-          setup     setup fossil configurations
-          config    fossil config management
+              list      lists existing archives
+              create    creates an archive from the panel
+              status    gets the status of an archive
+              restore   restores an archive to the panel
+              delete    deletes existing archives
+              setup     setup fossil configurations
+              config    fossil config management
 
       Global Options:
-          --no-color      disable ansi color codes
-          --trace         log error stack traces
-          -h, --help      get help information
-          -v, --version   get the current version
+              --no-color      disable ansi color codes
+              --trace         log error stack traces
+              -h, --help      get help information
+              -v, --version   get the current version
       HELP
     end
 
