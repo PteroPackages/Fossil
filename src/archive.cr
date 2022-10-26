c0370a4f058302b2b25f41b20715b54e34ec6696
devnote-dev <inferno0745@gmail.com>
2022-10-26 02:06:52 +0100

    refactor(archive): redo structures

--- src/archive.cr
+++ src/archive.cr
@@ -1,37 +1,20 @@
 module Fossil
   class Archive
-    include JSON::Serializable
+    property sources : Array(Source)
 
-    getter scopes : Array(String)
-    getter sourcemap : Hash(String, Array(String))
-    @[JSON::Field(ignore: true)]
-    getter sources : Array(Source(Models::BaseModel))
-
-    def initialize(@scopes, @sourcemap)
-      @sources = [] of Source(Models::BaseModel)
+    def initialize
+      @sources = [] of Source
     end
 
-    def self.create
-      new Array(String).new, Hash(String, Array(String)).new
-    end
-
-    def generate(path : Path) : Nil
-      if users = @sources.find { |s| s.key == "user" }
-        File.open(path / "frag_user_1.json") do |file|
-          IO.copy users.to_json, file
-        end
-      end
+    def generate(dir : Path) : Nil
     end
 
     struct Source(M)
       getter key : String
-      property meta : Hash(String, JSON::Any::Type)
-      property data : Array(M)
+      getter data : Array(M)
 
-      def initialize
+      def initialize(@data : Array(M))
         @key = M.class.name.downcase
-        @meta = {} of String => JSON::Any::Type
-        @data = [] of M
       end
     end
   end
