2f72c64c725124738467919dce412a89d9491348
devnote-dev <devnote.dev75@gmail.com>
2023-07-03 03:03:32 +0100

    feat(commands): move setup into env init

--- src/commands/env.cr
+++ src/commands/env.cr
@@ -3,6 +3,8 @@ module Fossil::Commands
     def setup : Nil
       @name = "env"
 
+      add_command Init.new
+
       add_argument "name"
     end
 
@@ -17,6 +19,42 @@ module Fossil::Commands
       else
         info "cache: #{Fossil::Config::CACHE_DIR}"
         info "library: #{Fossil::Config::LIBRARY_DIR}"
+      end
+    end
+  end
+
+  class Init < Base
+    def setup : Nil
+      @name = "init"
+    end
+
+    def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+      unless Dir.exists? Fossil::Config::CACHE_DIR
+        begin
+          Dir.mkdir_p Fossil::Config::CACHE_DIR
+        rescue ex
+          error "Failed to create Fossil cache directory:"
+          error ex
+        end
+      end
+
+      unless Dir.exists? Fossil::Config::LIBRARY_DIR
+        begin
+          Dir.mkdir_p Fossil::Config::LIBRARY_DIR
+        rescue ex
+          error "Failed to create Fossil library directory:"
+          error ex
+        end
+      end
+
+      conf = Fossil::Config::CACHE_DIR / "fossil.conf"
+      unless File.exists? conf
+        begin
+          File.touch conf
+        rescue ex
+          error "Failed to create Fossil configuration file:"
+          error ex
+        end
       end
     end
   end
